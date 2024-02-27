008259bcf8bf63c3428ed524a70d797f7f83a833
charliinew <charles.ignoux@epitech.eu>
2024-02-27 03:23:02 +0100

    [~]: modify the comment because it was not placed at the exact good byte

--- src/my_header.c
+++ src/my_header.c
@@ -15,13 +15,13 @@
 
 static void my_comment(char **line, int fd_new, header_t *header)
 {
-    u_int64_t test = 0;
+    int test = 0;
     header->prog_size = 0;
     my_strcpy(header->comment, line[1]);
     if (fd_new != -1) {
-        write(fd_new, &test, sizeof(test));
+        write(fd_new, &test, 3);
         write(fd_new, &header->prog_size, sizeof(header->prog_size));
-        write(fd_new, header->comment, sizeof (header->comment));
+        write(fd_new, header->comment, sizeof(header->comment));
     }
 }
 
@@ -48,13 +48,14 @@ void my_header(char **tab, int fd_new)
     header_t *header = malloc(sizeof(header_t));
 
     for (i = 0; tab[i] != NULL; i++)
-        data[i] = my_str_to_word_array_char(tab[i], "\"\t ");
+        data[i] = my_str_to_word_array_char(tab[i], "\"");
     data[i] = NULL;
     for (i = 0; data[i] != NULL && my_strncmp(data[i][0], ".name", 5) != 0;
     i++);
     if (data[i] != NULL)
         my_name(data[i], fd_new, header);
-    for (; data[i] != NULL && my_strncmp(data[i][0], ".comment", 8) != 0; i++);
+    for (i = 0; data[i] != NULL && my_strncmp(data[i][0], ".comment", 8) != 0;
+    i++);
     if (data[i] != NULL)
         my_comment(data[i], fd_new, header);
 }
